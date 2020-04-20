dbd1e11e22369a03876bf5c9a87f2057757750c2
richHarris0624 <47903767+richHarris0624@users.noreply.github.com>
2020-04-20 16:49:36 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { DatePicker } from "@progress/kendo-dateinputs-react-wrapper";
+import "./App.css";
+import "@progress/kendo-ui";
+import "@progress/kendo-theme-default/dist/all.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateTime: new Date()
+    };
+    this.onchange = this.onchange.bind(this);
+  }
+
+  onchange(e) {
+    console.log(e.sender.value());
+    this.setState({
+      dateTime: e.sender.value()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="date-picker">
+          <DatePicker
+            value={this.dateTime}
+            min={new Date(2017, 11, 1)}
+            max={new Date(2018, 11, 1)}
+            format={"yyyy/MM/dd"}
+            change={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
